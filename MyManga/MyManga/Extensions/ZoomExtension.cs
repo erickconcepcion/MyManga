a88defdef7f2c2ac7c592b642da805928a9bfaf5
Erick Concepcion <erickconcepcion@live.com>
2018-11-11 02:31:43 -0400

    Zoom component applyed

---
+++ MyManga/MyManga/Extensions/ZoomExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyManga.Extensions
+{
+    public static class ZoomExtension
+    {
+        public static double Clamp(this double self, double min, double max)
+        {
+            return Math.Min(max, Math.Max(self, min));
+        }
+    }
+}
